3b5ba64a338762854a32b0987506d2426f52a148
AdemHos <ademhos1907@outlook.com>
2025-07-22 15:21:56 +0300

    User Sync completed

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -2,8 +2,10 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import SignOutButton from '../conponents/SignOutButton'
+import { useUserSync } from '@/hooks/useUserSync'
 
 const HomeScreen = () => {
+  useUserSync()
   return (
     <SafeAreaView className='flex-'>
       <Text>Home Screen</Text>
